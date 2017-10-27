553fc1cd645b053b8acbb6cf5255cd0178871219
Mcghee-Jake <jmcghee@csumb.edu>
2017-10-27 10:22:45 -0700

    added lessRed function

--- MakeSmart_Lab3.py
+++ MakeSmart_Lab3.py
@@ -1,19 +1,35 @@
 # Warm Up
 
 def getPic():
-    """prompts a user to pick a file to be converted to a jython picture"""
+    """ prompts a user to pick a file to be converted to a jython picture """
     
     return makePicture(pickAFile())
 
-def halfRed():
-    """ Reduces red in a given image by 1/2 """
+def lessRed(percentage):
+    """ Reduces the red in an image by a given percentage 
+    
+    Args:
+        percentage (float): Amount of red that will be reduced in the image
+    
+    """
+    
+    if not (percentage > 0 and percentage < 1):
+        print("lessRed Error - Expected float between 0 and 1. \"%s\" given" % percentage)
+        return
     
     pic = getPic()
     pixels = getPixels(pic)
     for p in pixels:
         r = getRed(p)
-        setRed(p, r * .05)
+        setRed(p, r * percentage)
     repaint(pic)
+    
+    
+
+def halfRed():
+    """ Reduces red in a given image by 1/2 """
+    
+    lessRed(.5)
     
 def noBlue():
     """ Removes all blue values from a given image """
