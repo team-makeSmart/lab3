5402653c371d7c2e5c69e480689b37327a04dce5
Mcghee-Jake <jmcghee@csumb.edu>
2017-10-27 10:03:21 -0700

    added noBlue function

--- MakeSmart_Lab3.py
+++ MakeSmart_Lab3.py
@@ -1,17 +1,27 @@
 # Warm Up
 
 def getPic():
-    """prompts a user to pic a file to be converted to a jython picture"""
+    """prompts a user to pick a file to be converted to a jython picture"""
     
     return makePicture(pickAFile())
 
 def halfRed():
-    """ Reduces red in a chosen image by 1/2 """
+    """ Reduces red in a given image by 1/2 """
     
     pic = getPic()
     pixels = getPixels(pic)
     for p in pixels:
         r = getRed(p)
         setRed(p, r * .05)
+    repaint(pic)
+    
+def noBlue():
+    """ Removes all blue values from a given image """
+    
+    pic = getPic()
+    pixels = getPixels(pic)
+    for p in pixels:
+        b = getBlue(p)
+        setBlue(p, b * 0)
     repaint(pic)
     
